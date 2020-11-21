634f36fde680cb15bfb8085e06cf59307eeba244
Alpyne <dankparrot1@gmail.com>
2020-11-20 17:22:00 -0800

    Style fixup

--- lib/snowflake.c
+++ lib/snowflake.c
@@ -2,8 +2,7 @@
 
 #include <stdio.h>
 
-uint64_t read_snowflake(const char* string)
-{
+uint64_t read_snowflake(const char* string) {
     uint64_t snowflake = 0UL;
     sscanf(string, "%lu", &snowflake);
     return snowflake;
